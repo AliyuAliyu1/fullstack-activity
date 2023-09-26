3d2819defeede05d98acf7321c9538097a3a7f59
Aliyu Aliyu <aliyu@alumni.emu.edu.tr>
2023-09-26 04:03:42 +0100

    updadting

--- client/src/ListNode/Listnode.tsx
+++ client/src/ListNode/Listnode.tsx
@@ -1,20 +1,41 @@
-import React, {Fragment} from "react";
+import { todo } from "node:test";
+import React, {Fragment, useEffect,useState} from "react";
 
 const Listnode = () => {
-      
 
+    const [todos1,setTodos1] =useState([])
+    const getTodos = async () => {
+       
+
+        try {
+            const response = await fetch("http://localhost:3000/api/todos")
+            const jsondata = await response.json()
+            
+            setTodos1(jsondata)
+            
+        } catch (err) {
+            console.error(err.message)
+            
+        }
+    }
+      useEffect(() => {
+
+        getTodos()
+      },[])
+     
+console.log(todos1)
    return ( <Fragment>
        {" "}
    <table className="table mt-5 text-center">
       <thead>
         <tr>
-          <th>Firstname</th>
-          <th>Lastname</th>
-          <th>Email</th>
+          <th>Description</th>
+          <th>Delete</th>
+          <th>Edit</th>
         </tr>
       </thead>
       <tbody>
-        <tr>
+  {/*       <tr>
           <td>John</td>
           <td>Doe</td>
           <td>john@example.com</td>
@@ -23,12 +44,21 @@ const Listnode = () => {
           <td>Mary</td>
           <td>Moe</td>
           <td>mary@example.com</td>
-        </tr>
-        <tr>
-          <td>July</td>
-          <td>Dooley</td>
-          <td>july@example.com</td>
-        </tr>
+        </tr> */}
+       {todos1.map(todo => (
+
+            <tr>
+            <td>{todo.task}</td>
+            <td>Edit</td>
+            <td>
+               <button className="btn btn-danger">
+               Delete
+                </button> 
+                </td>
+            </tr>
+
+        ))} 
+ 
       </tbody>
     </table>
     </Fragment>
--- server/index.js
+++ server/index.js
@@ -55,7 +55,45 @@ app.post("/api/todos2", async (req, res) => {
 
   // res.send(data)
 });
+app.delete("/api/todos/:id", async (req, res) => {
+    
+  const { id} = req.params
+// 'Drink'. => ${task}
+const deleteQuery = await sql`DELETE FROM todos WHERE id = ${id}`;
 
+//  res.json(todos2)
+//   console.log(todos2);
+
+if (deleteQuery.count > 0) {
+  res.status(201).send(`Successfully deleted the row with ID ${id}`);
+} else {
+  res.status(404).send(`No row with ID ${id} was deleted.`);
+} 
+
+// res.send(data)
+});
+
+
+
+
+app.put("/api/todos/:id", async (req, res) => {
+    
+  const { id} = req.params
+  const { task} = req.body
+// 'Drink'. => ${task}
+const updateQuery = await sql`UPDATE todos SET task =${task}  WHERE id = ${id}`;
+
+//  res.json(todos2)
+//   console.log(todos2);
+
+if (updateQuery.count) {
+  res.status(201).send(`Successfully updated the row with ID ${id}`);
+} else {
+  res.status(404).send(`No row with ID ${id} was updated.`);
+} 
+
+// res.send(data)
+});
 app.listen(3000, () => {
   console.log("server runnning on port 3000");
 });
